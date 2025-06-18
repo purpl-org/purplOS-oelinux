2ab2c60558727cea233d152f677e23643f0d7444
OpenEmbedded <oe.patch@oe>
2025-06-17 22:37:28 -0400

    make the make-and-send-au.sh script copy the actual latest otas to a never-changing URL

--- build/make-and-send-au.sh
+++ build/make-and-send-au.sh
@@ -10,3 +10,6 @@ I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt o
 
 scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}d.ota" root@froggitti.xyz:/wire/otas/full/dev/0.3.1."${INCREMENT}".ota
 scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}oskr.ota" root@froggitti.xyz:/wire/otas/full/oskr/0.3.1."${INCREMENT}".ota
+
+scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}d.ota" root@froggitti.xyz:/wire/otas/full/latest/dev.ota
+scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}oskr.ota" root@froggitti.xyz:/wire/otas/full/latest/oskr.ota
