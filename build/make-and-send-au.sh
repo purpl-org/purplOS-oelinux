d3c47f0a1ec48456484ec6f10ea68df05b4ce444
OpenEmbedded <oe.patch@oe>
2025-06-17 22:35:58 -0400

    make the make-and-send-au.sh script copy the otas correctly

--- build/make-and-send-au.sh
+++ build/make-and-send-au.sh
@@ -8,5 +8,5 @@ export INCREMENT="$inc"
 I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt dev -v "$INCREMENT" -au
 I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v "$INCREMENT" -au
 
-scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}d.ota" root@froggitti.xyz:/wire/otas/full/dev/0.3.1."${INCREMENT}"d.ota
-scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}oskr.ota" root@froggitti.xyz:/wire/otas/full/oskr/0.3.1."${INCREMENT}"oskr.ota
+scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}d.ota" root@froggitti.xyz:/wire/otas/full/dev/0.3.1."${INCREMENT}".ota
+scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}oskr.ota" root@froggitti.xyz:/wire/otas/full/oskr/0.3.1."${INCREMENT}".ota
