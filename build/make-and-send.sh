021bd901b3e7a8b22660247862c0fd26d8d5fe25
OpenEmbedded <oe.patch@oe>
2025-06-17 21:46:57 -0400

    add make-and-send-au.sh

--- build/make-and-send.sh
+++ build/make-and-send.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 clear
-I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt dev -v 1 -au
-I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v 1 -au
-scp -P 23 -i ~/root_cozmoserver2 _build/vicos-0.3.1.1d.ota root@froggitti.xyz:/wire/otas/full/dev/0.3.1.1.ota
-scp -P 23 -i ~/root_cozmoserver2 _build/vicos-0.3.1.1oskr.ota root@froggitti.xyz:/wire/otas/full/oskr/0.3.1.1.ota
+
+read -p "enter build increment: " inc
+export INCREMENT="$inc"
+
+./build/build.sh -bt dev -v "$INCREMENT" 
+./build/build.sh -bt oskr -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v "$INCREMENT" 
+
+#scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}d.ota" root@froggitti.xyz:/wire/otas/full/dev/0.3.1."${INCREMENT}"d.ota
+#scp -P 23 -i ~/root_cozmoserver2 "_build/vicos-0.3.1.${INCREMENT}oskr.ota" root@froggitti.xyz:/wire/otas/full/oskr/0.3.1."${INCREMENT}"oskr.ota
