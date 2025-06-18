e6f46819a3c5cb7802eb608113f78f31d1e47fea
OpenEmbedded <oe.patch@oe>
2025-06-17 21:35:33 -0400

    Update everything

--- build/make-and-send.sh
+++ build/make-and-send.sh
@@ -1,1 +1,5 @@
-clear && I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt dev -v 0 -au && I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v 0 -au && scp -P 23 -i ~/root_cozmoserver2 _build/vicos-0.3.0.0d.ota root@froggitti.xyz:/wire/otas/full/dev/0.3.0.0.ota && scp -P 23 -i ~/root_cozmoserver2 _build/vicos-0.3.0.0oskr.ota root@froggitti.xyz:/wire/otas/full/oskr/0.3.0.0.ota
+clear
+I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt dev -v 1 -au
+I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v 1 -au
+scp -P 23 -i ~/root_cozmoserver2 _build/vicos-0.3.1.1d.ota root@froggitti.xyz:/wire/otas/full/dev/0.3.1.1.ota
+scp -P 23 -i ~/root_cozmoserver2 _build/vicos-0.3.1.1oskr.ota root@froggitti.xyz:/wire/otas/full/oskr/0.3.1.1.ota
