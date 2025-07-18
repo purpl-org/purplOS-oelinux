b52a783aab27f8d8fbda079ac79e98e1e20c7cc2
OpenEmbedded <oe.patch@oe>
2025-07-18 18:20:31 -0400

    make make-and-send-au.sh work, add a modified version of make-and-send-au.sh that lets me send pre-existing otas for convenience

--- build/send-built.sh
+++ build/send-built.sh
@@ -10,18 +10,9 @@ export INCREMENT="$inc"
 read -p "Enter path to server root key (required, FrogServer2): " keypath
 export KEY_PATH="$keypath"
 
-read -p "Enter OSKR bootloader password: " oskrpass
-export OSKR_PASSWORD="$oskrpass"
-
 eval `ssh-agent`
 
 ssh-add "$KEY_PATH"
-
-echo "Building Dev OTA with version 0.3.1.$INCREMENT"
-AUTO_UPDATE=1 ./build/build.sh -bt dev -v "$INCREMENT" -au
-
-echo "Building OSKR OTA with version 0.3.1.$INCREMENT"
-AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp "$OSKR_PASSWORD" -v "$INCREMENT" -au
 
 echo Touch auto update inhibitor
 ssh -p 23 root@froggitti.net 'touch /wire/otas/dnar'
@@ -40,10 +31,10 @@ ssh -p 23 root@froggitti.net "echo 0.3.1.$INCREMENT /wire/otas/latest"
 sleep 1s
 
 echo Copy Dev OTA
-scp -P 23 _build/vicos-0.3.1."$INCREMENT"d.ota root@froggitti.net:/wire/otas/full/dev/0.3.1."$INCREMENT".ota
+scp -P 23 ~/vector/purplOS-oelinux/_build/vicos-0.3.1."$INCREMENT"d.ota root@froggitti.net:/wire/otas/full/dev/0.3.1."$INCREMENT".ota
 
 echo Copy OSKR OTA
-scp -P 23 _build/vicos-0.3.1."$INCREMENT"oskr.ota root@froggitti.net:/wire/otas/full/oskr/0.3.1."$INCREMENT".ota
+scp -P 23 ~/vector/purplOS-oelinux/_build/vicos-0.3.1."$INCREMENT"oskr.ota root@froggitti.net:/wire/otas/full/oskr/0.3.1."$INCREMENT".ota
 
 echo Copy Dev OTA to a never-changing URL
 ssh -p 23 root@froggitti.net "cp /wire/otas/full/dev/0.3.1."$INCREMENT".ota /wire/otas/full/latest/dev.ota"
