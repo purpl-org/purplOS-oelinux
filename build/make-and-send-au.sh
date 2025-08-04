a2f72086c295b8d31307eb090a788efbe6977f02
OpenEmbedded <oe.patch@oe>
2025-08-04 00:58:13 -0400

    update script

--- build/make-and-send-au.sh
+++ build/make-and-send-au.sh
@@ -17,10 +17,10 @@ eval `ssh-agent`
 
 ssh-add "$KEY_PATH"
 
-echo "Building Dev OTA with version 0.3.1.$INCREMENT"
+echo "Building Dev OTA with version 0.3.2.$INCREMENT"
 AUTO_UPDATE=1 ./build/build.sh -bt dev -v "$INCREMENT" -au
 
-echo "Building OSKR OTA with version 0.3.1.$INCREMENT"
+echo "Building OSKR OTA with version 0.3.2.$INCREMENT"
 AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp "$OSKR_PASSWORD" -v "$INCREMENT" -au
 
 echo Touch auto update inhibitor
@@ -36,20 +36,20 @@ ssh -p 23 root@froggitti.net 'touch /wire/otas/latest'
 sleep 1s
 
 echo Echo new version number to new latest file
-ssh -p 23 root@froggitti.net "echo 0.3.1.$INCREMENT > /wire/otas/latest"
+ssh -p 23 root@froggitti.net "echo 0.3.2.$INCREMENT > /wire/otas/latest"
 sleep 1s
 
 echo Copy Dev OTA
-scp -P 23 _build/vicos-0.3.1."$INCREMENT"d.ota root@froggitti.net:/wire/otas/full/dev/0.3.1."$INCREMENT".ota
+scp -P 23 _build/vicos-0.3.2."$INCREMENT"d.ota root@froggitti.net:/wire/otas/full/dev/0.3.2."$INCREMENT".ota
 
 echo Copy OSKR OTA
-scp -P 23 _build/vicos-0.3.1."$INCREMENT"oskr.ota root@froggitti.net:/wire/otas/full/oskr/0.3.1."$INCREMENT".ota
+scp -P 23 _build/vicos-0.3.2."$INCREMENT"oskr.ota root@froggitti.net:/wire/otas/full/oskr/0.3.2."$INCREMENT".ota
 
 echo Copy Dev OTA to a never-changing URL
-ssh -p 23 root@froggitti.net "cp /wire/otas/full/dev/0.3.1."$INCREMENT".ota /wire/otas/full/latest/dev.ota"
+ssh -p 23 root@froggitti.net "cp /wire/otas/full/dev/0.3.2."$INCREMENT".ota /wire/otas/full/latest/dev.ota"
 	
 echo Copy OSKR OTA to a never-changing URL
-ssh -p 23 root@froggitti.net "cp /wire/otas/full/oskr/0.3.1."$INCREMENT".ota /wire/otas/full/latest/oskr.ota"
+ssh -p 23 root@froggitti.net "cp /wire/otas/full/oskr/0.3.2."$INCREMENT".ota /wire/otas/full/latest/oskr.ota"
 
 echo Remove auto update inhibitor
 ssh -p 23 root@froggitti.net 'rm /wire/otas/dnar'
