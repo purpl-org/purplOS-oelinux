fe15b825c0907713cb49153c116b7f4639beab70
froggitti <85637569+froggitti@users.noreply.github.com>
2025-09-19 16:39:06 -0400

    Update send-built.sh

--- build/send-built.sh
+++ build/send-built.sh
@@ -27,20 +27,20 @@ ssh -p 23 root@froggitti.net 'touch /wire/otas/latest && wall Latest file create
 sleep 1s
 
 echo Echo new version number to new latest file
-ssh -p 23 root@froggitti.net "echo 0.3.1.$INCREMENT >> /wire/otas/latest && wall Latest file updated."
+ssh -p 23 root@froggitti.net "echo 0.3.2.$INCREMENT >> /wire/otas/latest && wall Latest file updated."
 sleep 1s
 
 echo Copy Dev OTA
-scp -P 23 ~/vector/purplOS-oelinux/_build/vicos-0.3.1."$INCREMENT"d.ota root@froggitti.net:/wire/otas/full/dev/0.3.1."$INCREMENT".ota
+scp -P 23 ~/vector/purplOS-oelinux/_build/vicos-0.3.2."$INCREMENT"d.ota root@froggitti.net:/wire/otas/full/dev/0.3.2."$INCREMENT".ota
 
 echo Copy OSKR OTA
-scp -P 23 ~/vector/purplOS-oelinux/_build/vicos-0.3.1."$INCREMENT"oskr.ota root@froggitti.net:/wire/otas/full/oskr/0.3.1."$INCREMENT".ota
+scp -P 23 ~/vector/purplOS-oelinux/_build/vicos-0.3.2."$INCREMENT"oskr.ota root@froggitti.net:/wire/otas/full/oskr/0.3.2."$INCREMENT".ota
 
 echo Copy Dev OTA to a never-changing URL
-ssh -p 23 root@froggitti.net "cp /wire/otas/full/dev/0.3.1."$INCREMENT".ota /wire/otas/full/latest/dev.ota"
+ssh -p 23 root@froggitti.net "cp /wire/otas/full/dev/0.3.2."$INCREMENT".ota /wire/otas/full/latest/dev.ota"
 	
 echo Copy OSKR OTA to a never-changing URL
-ssh -p 23 root@froggitti.net "cp /wire/otas/full/oskr/0.3.1."$INCREMENT".ota /wire/otas/full/latest/oskr.ota"
+ssh -p 23 root@froggitti.net "cp /wire/otas/full/oskr/0.3.2."$INCREMENT".ota /wire/otas/full/latest/oskr.ota"
 
 echo Remove auto update inhibitor
 ssh -p 23 root@froggitti.net 'rm /wire/otas/dnar'
