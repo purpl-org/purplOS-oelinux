16901d1c276a28bcd45ef5bdd75fef61f389f84f
froggitti <milkysoibeans@gmail.com>
2026-03-02 01:33:52 -0500

    update purplpkg and fix build script

--- build/make-and-send-au.sh
+++ build/make-and-send-au.sh
@@ -23,7 +23,7 @@ echo "Building Dev OTA with version 0.3.3.$INCREMENT"
 AUTO_UPDATE=1 ./build/build.sh -bt dev -v "$INCREMENT" -au
 
 echo "Building OSKR OTA with version 0.3.3.$INCREMENT"
-AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v "$INCREMENT" -au
+AUTO_UPDATE=1 ./build/build.sh -bt oskr -s -op alike-essay-added-spumoni-lotion -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v "$INCREMENT" -au
 
 echo Touch auto update inhibitor
 ssh  root@www.froggitti.net 'touch /servers/drive4/froggitti-net/ota-purpl.skittl.net/otas/dnar'
@@ -42,16 +42,10 @@ ssh  root@www.froggitti.net "echo 0.3.3.$INCREMENT > /servers/drive4/froggitti-n
 sleep 1s
 
 echo Copy Dev OTA
-scp  _build/vicos-0.3.3."$INCREMENT"d.ota root@www.froggitti.net:/servers/drive4/froggitti-net/v.skittl.net/site/firmware/purplOS\ DEV/0.3.3."$INCREMENT".ota
+scp  _build/vicos-0.3.3."$INCREMENT"d.ota root@www.froggitti.net:/servers/drive4/froggitti-net/ota-purpl.skittl.net/otas/full/dev/0.3.3."$INCREMENT".ota
 
 echo Copy OSKR OTA
-scp  _build/vicos-0.3.3."$INCREMENT"oskr.ota root@www.froggitti.net:/servers/drive4/froggitti-net/v.skittl.net/site/firmware/purplOS\ OSKR/0.3.3."$INCREMENT".ota
-
-#echo Copy Dev OTA to a never-changing URL
-#ssh  root@www.froggitti.net "cp /servers/drive4/froggitti-net/v.skittl.net/site/firmware/purplOS\ DEV//0.3.3."$INCREMENT".ota /servers/drive4/froggitti-net/ota-purpl.skittl.net/otas/full/latest/dev.ota"
-	
-#echo Copy OSKR OTA to a never-changing URL
-#ssh  root@www.froggitti.net "cp /servers/drive4/froggitti-net/ota-purpl.skittl.net/otas/full/oskr/0.3.3."$INCREMENT".ota /servers/drive4/froggitti-net/ota-purpl.skittl.net/otas/full/latest/oskr.ota"
+scp  _build/vicos-0.3.3."$INCREMENT"oskr.ota root@www.froggitti.net:/servers/drive4/froggitti-net/ota-purpl.skittl.net/otas/full/oskr/0.3.3."$INCREMENT".ota
 
 echo Remove auto update inhibitor
 ssh  root@www.froggitti.net 'rm /servers/drive4/froggitti-net/ota-purpl.skittl.net/otas/dnar'
